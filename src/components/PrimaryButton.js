75f7f6dbdab30f1dc0014b4c93828ed1607efd6b
Harry Frankel <hfranks92@gmail.com>
2020-05-20 10:56:45 +1000

    Home page 1.0 complete

--- src/components/PrimaryButton.js
+++ src/components/PrimaryButton.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './../assets/styles/primaryButton.css';
 
-const PrimaryButton = () => {
-  return <button>Hello World</button>;
+const PrimaryButton = (props) => {
+  return <button className="primary-button">{props.buttonTitle}</button>;
 };
 
 export default PrimaryButton;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PrimaryButton from './../components/PrimaryButton';
 import ListCheck from './../assets/icons/list-check.svg';
 import Trophy from './../assets/icons/trophy.svg';
 import './../assets/styles/homePage.css';
 
 const HomePage = () => {
   return (
-    <section>
-      <div className="home-page-slug">
-        <img src={ListCheck} alt="Checklist icon" />
-        <h3>Improve your JavaScript Skills</h3>
-      </div>
-      <div className="home-page-slug">
-        <img src={Trophy} alt="Trophy icon" />
-        <h3>Win bragging rights with a top score</h3>
-      </div>
-    </section>
+    <>
+      <section className="home-page-main">
+        <div className="home-page-slug">
+          <img src={ListCheck} alt="Checklist icon" />
+          <h3>Improve your JavaScript Skills</h3>
+        </div>
+        <div className="home-page-slug">
+          <img src={Trophy} alt="Trophy icon" />
+          <h3>Win bragging rights with top score</h3>
+        </div>
+      </section>
+      <section className="home-page-buttons">
+        <Link to="/howitworks">
+          <PrimaryButton buttonTitle="How It Works" />
+        </Link>
+        <PrimaryButton buttonTitle="Leaderboard" />
+      </section>
+    </>
   );
 };
 
