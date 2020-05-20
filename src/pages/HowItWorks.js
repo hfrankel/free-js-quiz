10d151dba50aa97930ae3a428ecf777ba2da45b9
Harry Frankel <hfranks92@gmail.com>
2020-05-20 11:42:00 +1000

    Experiment with svg arrow

--- src/pages/HowItWorks.js
+++ src/pages/HowItWorks.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Arrow from './../assets/icons/arrow-down-right.svg';
+import './../assets/styles/howItWorksPage.css';
 
 const HowItWorksPage = () => {
   return (
-    <div>
-      <h1>This is the how it works page</h1>
-    </div>
+    <section>
+      <h3>Select the rights answers</h3>
+      <img src={Arrow} alt="Arrow" />
+      <h3>Get harder questions</h3>
+      <h3>Select the wrong answers</h3>
+      <h3>Get easier questions</h3>
+    </section>
   );
 };
 
