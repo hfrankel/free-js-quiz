d1f33d7bd03170d5d7ef0b09c42444d921fb8e44
Harry Frankel <hfranks92@gmail.com>
2020-05-20 14:37:45 +1000

    Added start quiz button

--- src/pages/HowItWorks.js
+++ src/pages/HowItWorks.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import PrimaryButton from './../components/PrimaryButton';
 import Arrow from './../assets/icons/arrow-down-right.svg';
 import './../assets/styles/howItWorksPage.css';
 
 const HowItWorksPage = () => {
   return (
-    <section>
-      <h3>Select the rights answers</h3>
-      <img src={Arrow} alt="Arrow" />
-      <h3>Get harder questions</h3>
-      <h3>Select the wrong answers</h3>
-      <h3>Get easier questions</h3>
-    </section>
+    <>
+      <section>
+        <h3>Select the rights answers</h3>
+        {/* <img src={Arrow} alt="Arrow" /> */}
+        <h3>Get harder questions</h3>
+        <h3>Select the wrong answers</h3>
+        <h3>Get easier questions</h3>
+      </section>
+      <section>
+        <PrimaryButton buttonTitle="Start Quiz" />
+      </section>
+    </>
   );
 };
 
