2ab5d6a7f819936eb1eda214c2b9fcfb502dc20c
Harry Frankel <hfranks92@gmail.com>
2020-05-22 12:27:04 +1000

    Added new how it works test svg

--- src/pages/HowItWorks.js
+++ src/pages/HowItWorks.js
@@ -11,20 +11,16 @@ const HowItWorksPage = () => {
         <h3>Select the rights answers</h3>
         {/* <img src={Arrow} alt="Arrow" /> */}
         <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="icon icon-tabler icon-tabler-arrow-down-right"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          stroke-width="2"
-          stroke="currentColor"
+          width="129"
+          height="142"
+          viewBox="0 0 129 142"
           fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          <path stroke="none" d="M0 0h24v24H0z" />
-          <line x1="7" y1="7" x2="17" y2="17" />
-          <polyline points="17 8 17 17 8 17" />
+          <path
+            d="M127.245 120.691C127.486 120.556 127.571 120.251 127.436 120.01L125.233 116.086C125.097 115.846 124.793 115.76 124.552 115.895C124.311 116.03 124.225 116.335 124.361 116.576L126.319 120.064L122.832 122.022C122.591 122.157 122.505 122.462 122.64 122.703C122.776 122.944 123.08 123.029 123.321 122.894L127.245 120.691ZM24.6775 2.03927C27.6185 39.366 35.3211 64.0732 51.267 81.9422C67.2051 99.8024 91.3118 110.754 126.865 120.736L127.135 119.773C91.618 109.801 67.7506 98.9118 52.0131 81.2764C36.2835 63.6498 28.6091 39.2068 25.6745 1.96073L24.6775 2.03927Z"
+            fill="black"
+          />
         </svg>
         <h3>Get harder questions</h3>
         <h3>Select the wrong answers</h3>
