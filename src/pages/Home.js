918522024fa1d0e6339680d20c1bd9ec8cf792af
Harry Frankel <hfranks92@gmail.com>
2020-05-27 12:30:56 +1000

    Reformatted home page to use css grids

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,22 +8,24 @@ import './../assets/styles/homePage.css';
 const HomePage = () => {
   return (
     <>
-      <section className="home-page-main">
-        <div className="home-page-slug">
-          <img src={ListCheck} alt="Checklist icon" />
-          <h3>Improve your JavaScript Skills</h3>
-        </div>
-        <div className="home-page-slug">
-          <img src={Trophy} alt="Trophy icon" />
-          <h3>Win bragging rights with top score</h3>
-        </div>
-      </section>
-      <section className="home-page-buttons">
-        <Link to="/howitworks">
-          <PrimaryButton buttonTitle="How It Works" />
-        </Link>
-        <PrimaryButton buttonTitle="Leaderboard" />
-      </section>
+      <div className="home-page-grid">
+        <section className="home-page-main">
+          <div className="home-page-slug">
+            <img src={ListCheck} alt="Checklist icon" />
+            <h3>Improve your JavaScript Skills</h3>
+          </div>
+          <div className="home-page-slug">
+            <img src={Trophy} alt="Trophy icon" />
+            <h3>Win bragging rights with top score</h3>
+          </div>
+        </section>
+        <section className="home-page-buttons">
+          <Link to="/howitworks">
+            <PrimaryButton buttonTitle="How It Works" />
+          </Link>
+          <PrimaryButton buttonTitle="Leaderboard" />
+        </section>
+      </div>
     </>
   );
 };
